8c58d1274d11ec5edafa2897e6034b82517fb378
mateo <mateo1559@gmail.com>
2025-02-25 17:14:08 +0100

    feat: Add day 1 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,11 +1,81 @@
+use utils::read_lines;
+use std::iter::zip;
+use std::collections::HashMap;
+use std::io::Lines;
+use std::io::BufReader;
+use std::fs::File;
 fn main() {
-    let input = std::fs::read_to_string("inputs/day01.txt")
-        .expect("Failed to read input file");
-    // Process the input
-    println!("Solution: {}", solve(input));
+    let file = read_lines("inputs/day01.txt");
+
+    let lines = match file {
+        Ok(file) => file,
+        Err(e) => panic!("Error reading file: {}", e),
+    };
+
+    second_part(lines);
 }
 
-fn solve(input: String) -> i32 {
-    // Your solution logic
-    42
+
+fn first_part(lines: Lines<BufReader<File>>) {
+    let mut first_column: Vec<i32> = vec![];
+    let mut second_column: Vec<i32> = vec![];
+
+    for line_res in lines {
+        if let Ok(line) = line_res {
+            let nums: Vec<i32> = line
+                .split_whitespace() // Split by spaces
+                .map(|s| s.parse().unwrap()) // Convert to numbers
+                .collect();
+
+            first_column.push(nums[0]);
+            second_column.push(nums[1]);
+        }
+    }
+
+    first_column.sort();
+    second_column.sort();
+
+    let  mut sum = 0;
+
+    for  (i,j) in zip(first_column, second_column) {
+        sum += i32::abs(i-j);
+    }
+
+    println!("Sum: {}", sum);
+
+}
+
+
+fn second_part(lines: Lines<BufReader<File>>) {
+    let mut occurances = HashMap::new();
+
+    let mut first_column: Vec<i32> = vec![];
+
+    for line_res in lines {
+        if let Ok(line) = line_res {
+            let nums: Vec<i32> = line
+                .split_whitespace() // Split by spaces
+                .map(|s| s.parse().unwrap()) // Convert to numbers
+                .collect();
+                    first_column.push(nums[0]);
+                let second_num = nums[1];
+
+                if occurances.contains_key(&second_num) {
+                    occurances.insert(second_num, occurances.get(&second_num).unwrap() + 1);
+                } else {
+                    occurances.insert(second_num, 1);
+                }
+        }
+    }
+
+    let mut sum = 0;
+    for i in first_column {
+        if occurances.contains_key(&i) {
+            sum += occurances.get(&i).unwrap() * i;
+        }
+    }
+
+    println!("Sum: {}", sum);
+
+
 }
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fs;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
+
+// Read entire file as a string
+ pub fn read_input(s: &str) -> String {
+    fs::read_to_string(s).expect("Could not read file")
 }
+
+// Read file line by line
+pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<BufReader<fs::File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = fs::File::open(filename)?;
+    Ok(BufReader::new(file).lines())
+}
+
+/*     let file =  read_lines("inputs/day01.txt");
+
+    let lines = match file {
+        Ok(file) => file,
+        Err(e) => panic!("Error reading file: {}", e),
+    };
+
+   
+    for line_res in lines {
+        if let Ok(line) = line_res {
+            println!("Line: {}", line);
+        } */
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_read_input() {
+        let input = read_input("inputs/day01.txt");
+        assert_eq!(input.len(), 100);
+    }
+
+    #[test]
+    fn test_read_lines() {
+        let lines = read_lines("inputs/day01.txt").unwrap();
+        let vec: Vec<String> = lines.map(|l| l.unwrap()).collect();
+        assert_eq!(vec.len(), 100);
     }
 }
